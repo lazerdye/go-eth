e5465bfcd27d2040af6a9804c4bab5436668ed88
Terence Haddock <thaddock@tripi.com>
2020-03-07 18:43:52 -0500

    Zeroex: Add missing cli file.

---
+++ cli/zeroex.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/big"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/lazerdye/go-eth/wallet"
+	"github.com/lazerdye/go-eth/zeroex"
+)
+
+var (
+	clientZeroexCommand       = clientCmd.Command("zeroex", "Zeroex operations")
+	clientZeroexDeposit       = clientZeroexCommand.Command("deposit", "Deposit eth into zeroex")
+	clientZeroexDepositAmount = clientZeroexDeposit.Arg("amount", "Amount to deposit").Required().String()
+	clientZeroexBalanceOf     = clientZeroexCommand.Command("balanceof", "Eth (weth) balance for transactions")
+)
+
+func zeroexDepositCommand(client *zeroex.Client, account *wallet.Account) error {
+	amount, _, err := new(big.Float).Parse(*clientZeroexDepositAmount, 10)
+	if err != nil {
+		return err
+	}
+	log.Infof("Amount: %s", amount.String())
+	tx, err := client.EtherTokenDeposit(context.Background(), account, amount)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Result: %s\n", tx.Hash().String())
+	return nil
+}
+
+func zeroexBalanceOfCommand(client *zeroex.Client, account *wallet.Account) error {
+	amount, err := client.EtherTokenBalanceOf(context.Background(), account)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Balance of account %s: %s\n", account.Address().String(), amount.String())
+	return nil
+}
