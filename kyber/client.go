a067caa51bcaa379120f9db61bdde2fa9c736c20
Terence Haddock <thaddock@tripi.com>
2020-05-02 06:53:17 -0400

    Kyber: Increase gas limit

--- kyber/client.go
+++ kyber/client.go
@@ -23,7 +23,7 @@ const (
 )
 
 var (
-	tradeGasLimit            = uint64(400000)
+	tradeGasLimit            = uint64(700000)
 	KyberNetworkProxyAddress = common.HexToAddress(KyberNetworkProxyAddressString)
 	EthereumAddress          = common.HexToAddress(EthereumAddressString)
 	ethAmounts               = []float64{0.2, 0.5, 1.0, 10.0}
