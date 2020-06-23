3d5f85fcda7ff920b77fe42e9b119d98396e4005
Terence Haddock <thaddock@tripi.com>
2020-06-23 09:07:30 +0000

    Compound: Implement cbat

--- cli/compound.go
+++ cli/compound.go
@@ -14,24 +14,41 @@ import (
 
 var (
 	clientCompoundCommand = clientToken2Command.Command("compound", "Compound operations")
+	clientCompoundToken   = clientCompoundCommand.Flag("token", "Token for compound").String()
 	clientMintCommand     = clientCompoundCommand.Command("mint", "Mint currency")
-	clientMintToken       = clientMintCommand.Arg("token", "Token to mint").Required().String()
 	clientMintAmount      = clientMintCommand.Arg("amount", "Amount to mint").Required().Float64()
 )
 
 func compoundCommands(ctx context.Context, client *client.Client, reg *token2.Registry, commands []string) (bool, error) {
+	var compoundClient compound.Client
+	var err error
+	switch *clientCompoundToken {
+	case "eth", "":
+		compoundClient, err = compound.NewEthClient(client)
+		if err != nil {
+			return false, err
+		}
+	case "bat":
+		tok, err := reg.ByName(*clientCompoundToken)
+		if err != nil {
+			return false, err
+		}
+		compoundClient, err = compound.NewErc20Client(client, tok)
+		if err != nil {
+			return false, err
+		}
+	default:
+		return false, errors.Errorf("Unsupported token: %s", *clientCompoundToken)
+	}
+
 	switch commands[0] {
 	case "mint":
-		return true, compoundMint(ctx, client)
+		return true, compoundMint(ctx, compoundClient)
 	}
 	return false, nil
 }
 
-func compoundMint(ctx context.Context, client *client.Client) error {
-	compoundClient, err := compound.NewClient(client, *clientMintToken)
-	if err != nil {
-		return err
-	}
+func compoundMint(ctx context.Context, compoundClient compound.Client) error {
 	account, opened, err := getAccount()
 	if err != nil {
 		return err
@@ -39,7 +56,6 @@ func compoundMint(ctx context.Context, client *client.Client) error {
 	if !opened {
 		return errors.New("Account not unlocked")
 	}
-
 
 	amountBig := decimal.NewFromFloat(*clientMintAmount)
 	tx, err := compoundClient.Mint(ctx, account, amountBig)
--- compound/client.go
+++ compound/client.go
@@ -5,11 +5,12 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/pkg/errors"
+	//"github.com/pkg/errors"
 	"github.com/shopspring/decimal"
 
 	"github.com/lazerdye/go-eth/client"
 	"github.com/lazerdye/go-eth/gasstation"
+	"github.com/lazerdye/go-eth/token2"
 	"github.com/lazerdye/go-eth/wallet"
 )
 
@@ -17,9 +18,10 @@ const ()
 
 var (
 	CethContractAddress = common.HexToAddress("0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5")
+	CbatContractAddress = common.HexToAddress("0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e")
 
-	mintGasSpeed = gasstation.Average
-	mintGasLimit = uint64(150000)
+	mintGasSpeed = gasstation.Fast
+	mintGasLimit = uint64(300000)
 )
 
 type Client interface {
@@ -32,15 +34,27 @@ type cethClient struct {
 	ceth *Ceth
 }
 
-func NewClient(client *client.Client, currency string) (Client, error) { 
-	if currency != "eth" {
-		return nil, errors.Errorf("Unsupported currency: %s", currency)
-	}
+type cerc20Client struct {
+	*client.Client
+
+	tok    *token2.Client
+	cerc20 *CErc20
+}
+
+func NewEthClient(client *client.Client) (Client, error) {
 	ceth, err := NewCeth(CethContractAddress, client)
 	if err != nil {
 		return nil, err
 	}
 	return &cethClient{client, ceth}, nil
+}
+
+func NewErc20Client(client *client.Client, tok *token2.Client) (Client, error) {
+	cerc20, err := NewCErc20(CbatContractAddress, client)
+	if err != nil {
+		return nil, err
+	}
+	return &cerc20Client{client, tok, cerc20}, nil
 }
 
 func (c *cethClient) Mint(ctx context.Context, account *wallet.Account, amount decimal.Decimal) (*types.Transaction, error) {
@@ -54,4 +68,21 @@ func (c *cethClient) Mint(ctx context.Context, account *wallet.Account, amount d
 	}
 
 	return c.ceth.Mint(opts)
+}
+
+func (c *cerc20Client) Mint(ctx context.Context, account *wallet.Account, amount decimal.Decimal) (*types.Transaction, error) {
+	gasPrice, _, err := c.GasPrice(ctx, mintGasSpeed)
+	if err != nil {
+		return nil, err
+	}
+	opts, err := account.NewTransactor(ctx, nil, gasPrice, mintGasLimit)
+	if err != nil {
+		return nil, err
+	}
+	amountWei, err := c.tok.ToWeiCapped(ctx, amount, account)
+	if err != nil {
+		return nil, err
+	}
+
+	return c.cerc20.Mint(opts, amountWei)
 }
