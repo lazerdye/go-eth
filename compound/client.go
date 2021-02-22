72fc34c5d7bdfed068675040b12524b1a6f82812
Terence Haddock <thaddock@tripi.com>
2021-02-22 12:32:40 +0000

    Add usdc, usdt compound

--- compound/client.go
+++ compound/client.go
@@ -21,6 +21,8 @@ var (
 	compoundContractAddresses = map[string]common.Address{
 		"bat": common.HexToAddress("0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e"),
 		"zrx": common.HexToAddress("0xb3319f5d18bc0d84dd1b4825dcde5d5f7266d407"),
+		"usdc": common.HexToAddress("0x39aa39c021dfbae8fac545936693ac917d5e7563"),
+		"usdt": common.HexToAddress("0xf650c3d88d12db855b8bf7d11be6c55a4e07dcc9"),
 	}
 
 	mintGasSpeed = gasoracle.Fast
